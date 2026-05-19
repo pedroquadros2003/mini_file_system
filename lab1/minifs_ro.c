5284c3331a1afea748399a550c1ddd3cb7cc86cf
PedroUlisses <peu.quadros@gmail.com>
2026-05-19 15:24:13 -0300

    fix: create a makefile for lab1

---
+++ lab1/minifs_ro.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#define FUSE_USE_VERSION 31
+#include <fcntl.h>
+#include <fuse3/fuse.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv) { return 0; }
